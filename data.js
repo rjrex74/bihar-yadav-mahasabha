e6a72bf6e455afb9c5a8b99ff10485d80f35649c
rjrex74 <rjrex74u@gmail.com>
2026-08-02 23:49:28 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,66 @@
+// src/data.js
+
+export const CORE_COMMITTEE = [
+  { 
+    id: 1, 
+    name: 'Balmiki Yadav', 
+    role: 'Leadership & Political Management', 
+    desc: 'Guiding organizational policy, political strategy, and core leadership.' 
+  },
+  { 
+    id: 2, 
+    name: 'Roshan Yadav', 
+    role: 'Organization Management', 
+    desc: 'Managing core operations and structural integrity of the organization.' 
+  },
+  { 
+    id: 3, 
+    name: 'Anjesh Yadav', 
+    role: 'All District Teams Co-ordinator', 
+    desc: 'Coordinating district-level teams, expansion, and grassroots connectivity.' 
+  },
+  { 
+    id: 4, 
+    name: 'Bobby Yadav', 
+    role: 'Management Head', 
+    desc: 'Overseeing general administration and high-level management decisions.' 
+  },
+  { 
+    id: 5, 
+    name: 'Roshan Yadav', 
+    role: 'Event Organization Head', 
+    desc: 'Executing and managing large-scale organizational events and campaigns.' 
+  },
+  { 
+    id: 6, 
+    name: 'Shivdatt Yadav', 
+    role: 'Social Media Management', 
+    desc: 'Leading the Social media, digital presence, and press management.' 
+  },
+  { 
+    id: 7, 
+    name: 'Rajneesh Kumar', 
+    role: 'Technical', 
+    desc: 'Leading the technical infrastructure, digital presence, and platform innovation.' 
+  },
+  { 
+    id: 8, 
+    name: 'Navin Yadav', 
+    role: 'Group Management', 
+    desc: 'Leading the management of all the whatsapp and other social media groups.' 
+  },
+  { 
+    id: 9, 
+    name: 'Sachin Yadav', 
+    role: 'Press and Editorial', 
+    desc: 'Leading the social media content writting and press management.' 
+  },
+  { 
+    id: 10, 
+    name: 'Pintu Kumar Yadav', 
+    role: 'Transportation and movement Head', 
+    desc: 'Leads all transportation related matters' 
+  },
+];
+
+export const OFFICE_ADDRESS = "यादव बैठकी ( MSS माधव शक्ति संगठन ): 📍 पटना, कुम्हरार, दाऊदबिगहा (महावीर स्थान) near Mahavir Asthan, Daud Bigha, Mahatma Gandhi Nagar, Kumhrar, Patna, Bihar 800026";
